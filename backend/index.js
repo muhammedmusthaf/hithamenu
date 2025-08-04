ca81ab6127a8bdb89dd8f0b04cc8ae2dde2949b2
muhammedmusthaf <muhammedmusthaf02@gmail.com>
2025-08-04 12:05:11 +0530

    addf

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "https://hithamenu.vercel.app/","https://hithaadmin.vercel.app/"
-    ]
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
 
 app.use("/api/menu", menu);
