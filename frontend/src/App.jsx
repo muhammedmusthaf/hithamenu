39e3caf7f4b7f59063393f5b7234e8a08fec93d6
muhammedmusthaf <muhammedmusthaf02@gmail.com>
2025-08-07 01:48:51 +0530

    cssww

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Menu from './components/Menu.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/menu" />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/menu/:categoryId" element={<Menu />} />
       </Routes>
